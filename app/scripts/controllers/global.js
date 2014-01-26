ad1ed562dcd1f5cef89c2d2aab5fa469b71096be
metallimax <maxime.delrieu@gmail.com>
2014-01-26 17:59:19 +0100

    quick change

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -2,9 +2,6 @@
 
 angular.module('controllers')
   .controller('GlobalCtrl', ['$scope', '$route', function ($scope, $route) {
-    // $scope.current = $route.current.$$route.originalPath;
-    // $log.info($route);
-    // $log.info($scope.current);
     $scope.$on('$routeChangeSuccess', function() {
       $scope.route = $route.current.$$route.originalPath;
     });
