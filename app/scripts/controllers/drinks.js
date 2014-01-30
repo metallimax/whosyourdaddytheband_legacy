52fee9e2a4b954d366628087d502051f905efd23
unknown <1di@PCS8556W8.GVA.ICRC.Priv>
2014-01-30 11:44:59 +0100

    drinks

--- app/scripts/controllers/drinks.js
+++ app/scripts/controllers/drinks.js
@@ -2,9 +2,41 @@
 
 angular.module('whosyourdaddythebandApp')
   .controller('DrinksCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.drinks = [
+      {
+        type: 'Beers',
+        items: [
+          {
+            name: 'Leffe',
+            logo: 'images/drinks/beer-leffe.png',
+            url: 'http://www.leffe.com',
+          },
+          {
+            name: 'La Goudale',
+            logo: 'images/drinks/beer-goudale.png',
+            url: 'http://www.brasseurs-gayant.com/portfolio/la-goudale',
+          },
+          {
+            name: 'Jeanlain',
+            logo: 'images/drinks/beer-jenlain.png',
+            url: 'http://jenlain.fr',
+          }
+        ]
+      },
+      {
+        type: 'Whisk(e)y',
+        items: [
+          {
+            name: 'Jack Daniel\'s',
+            logo: 'images/drinks/whiskey-jd.jpg',
+            url: 'http://www.jackdaniels.com',
+          },
+          {
+            name: 'Connemara',
+            logo: 'images/drinks/whiskey-connemara.jpg',
+            url: 'http://www.connemarawhiskey.com',
+          }
+        ]
+      }
     ];
   });
