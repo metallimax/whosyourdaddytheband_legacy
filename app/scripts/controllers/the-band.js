ea961da62f79bd3698ad6c1e607eeadd8676baa7
unknown <1di@PCS8556W8.GVA.ICRC.Priv>
2014-01-30 11:12:14 +0100

    header + band refactor

--- app/scripts/controllers/the-band.js
+++ app/scripts/controllers/the-band.js
@@ -9,50 +9,74 @@ angular.module('controllers')
         avatar: 'images/profiles/max.png',
         bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, molestias et commodi veritatis cupiditate reprehenderit quibusdam dolorum esse saepe illum excepturi vel aliquam temporibus molestiae natus necessitatibus aliquid quis in!',
         instruments: ['Guitar', 'Vocals'],
-        gears: {
-          'Guitars': ['Jackson RR', 'Jackson Kelly'],
-          'Effects': [
-            'Dunlop Crybaby Wha',
-            'Visual Sound Double Trouble',
-            'Boss Harmonist PS-6',
-            'MXR Phaser Phase 90',
-            'T-Rex Tonebug Chorus/Flanger',
-            'MXR Boost MC-401',
-            'G-Lab Smooth Delay SD-1',
-            'Boss Metal Zone MT-2'
-          ],
-          'Amplifiers': [
-            'Marshall JCM 2000 DSL 401',
-            'Peavey Ultra+ w/ Marshall 1960AV Cabinet'
-          ],
-          'Looper/Switcher': [
-            'G-Lab GSC-3',
-            'G-Lab MIDI 2x Loop M2L (x2)'
-          ],
-          'Other': [
-            'tc electronic Polytune Tuner',
-            'ISP Noise Gate Decimator G String'
-          ]
-        }
+        gears: [
+          {
+            type: 'Guitars',
+            gears: ['Jackson RR', 'Jackson Kelly']
+          },
+          {
+            type: 'Amplifiers',
+            gears: [
+              'Marshall JCM 2000 DSL 401',
+              'Peavey Ultra+ w/ Marshall 1960AV Cabinet'
+            ]
+          },
+          {
+            type: 'Effects',
+            gears: [
+              'Dunlop Crybaby Wha',
+              'Visual Sound Double Trouble',
+              'Boss Harmonist PS-6',
+              'MXR Phaser Phase 90',
+              'T-Rex Tonebug Chorus/Flanger',
+              'MXR Boost MC-401',
+              'G-Lab Smooth Delay SD-1',
+              'Boss Metal Zone MT-2'
+            ]
+          },
+          {
+            type: 'Looper/Switcher',
+            gears: [
+              'G-Lab GSC-3',
+              'G-Lab MIDI 2x Loop M2L (x2)'
+            ]
+          },
+          {
+            type: 'Other',
+            gears: [
+              'tc electronic Polytune Tuner',
+              'ISP Noise Gate Decimator G String'
+            ]
+          }
+        ]
       },
       {
         name: 'David',
         avatar: 'images/profiles/david.png',
         bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, molestias et commodi veritatis cupiditate reprehenderit quibusdam dolorum esse saepe illum excepturi vel aliquam temporibus molestiae natus necessitatibus aliquid quis in!',
         instruments: ['Drums'],
-        gears: {
-          'Drum Kit': ['??']
-        }
+        gears: [
+          {
+            type: 'Drum Kit',
+            gears: ['??']
+          }
+        ]
       },
       {
         name: 'Benji',
         avatar: 'images/profiles/benji.png',
         bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, molestias et commodi veritatis cupiditate reprehenderit quibusdam dolorum esse saepe illum excepturi vel aliquam temporibus molestiae natus necessitatibus aliquid quis in!',
         instruments: ['Bass'],
-        gears: {
-          'Bass': ['LTD B-1005'],
-          'Effects': ['Dunlop Bass Q Crybaby Wha', 'Bass Overdrive Boss ODB-3']
-        }
+        gears: [
+          {
+            type: 'Bass',
+            gears: ['LTD B-1005']
+          },
+          {
+            type: 'Effects',
+            gears: ['Dunlop Bass Q Crybaby Wha', 'Bass Overdrive Boss ODB-3']
+          }
+        ]
       }]
     };
   }]);
