f4e9b9060003ceae803edcc794b520f2d7fa292d
unknown <1di@PCS8556W8.GVA.ICRC.Priv>
2014-02-12 11:19:51 +0100

    images in CSS

--- app/scripts/controllers/drinks.js
+++ app/scripts/controllers/drinks.js
@@ -8,19 +8,16 @@ angular.module('whosyourdaddythebandApp')
         items: [
           {
             name: 'Leffe',
-            logo: 'images/drinks/beer-leffe.png',
             id: 'drink-leffe',
             url: 'http://www.leffe.com'
           },
           {
             name: 'La Goudale',
-            logo: 'images/drinks/beer-goudale.png',
             id: 'drink-goudale',
             url: 'http://www.brasseurs-gayant.com/portfolio/la-goudale'
           },
           {
             name: 'Jeanlain',
-            logo: 'images/drinks/beer-jenlain.png',
             id: 'drink-jeanlain',
             url: 'http://jenlain.fr'
           }
@@ -31,13 +28,11 @@ angular.module('whosyourdaddythebandApp')
         items: [
           {
             name: 'Jack Daniel\'s',
-            logo: 'images/drinks/whiskey-jd.jpg',
             id: 'drink-jd',
             url: 'http://www.jackdaniels.com'
           },
           {
             name: 'Connemara',
-            logo: 'images/drinks/whiskey-connemara.jpg',
             id: 'drink-connemara',
             url: 'http://www.connemarawhiskey.com'
           }
