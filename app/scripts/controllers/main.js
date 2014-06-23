a13b81c019936e6f6bf051cc58910f4663354bcb
unknown <1di@PCS8556W8.GVA.ICRC.Priv>
2014-06-23 14:34:24 +0200

    rajout dd

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,12 @@ angular.module('controllers')
         author: 'Max',
         title: 'Welcome to the new band and website design',
         content: 'We recently changed the design of the website'
+      },
+      {
+        date: '2014-06-21',
+        author: 'Max',
+        title: 'Who\'s Your Daddy? @ Fête de la Musique d\'Albertville',
+        content: '1st gig'
       }
     ];
   }]);
--- app/scripts/controllers/the-band.js
+++ app/scripts/controllers/the-band.js
@@ -10,16 +10,16 @@ angular.module('controllers')
         'After a while looking for partners, they finally found a drummer (in the person of David) that matched with Max spirit.',
         'Unfortunately, Sylvain thought that the style went too heavy/fast so he decided to left the band.',
         'Benji, a former neighboor of David who is a bass player was involved in the challenge of this young band.',
-        'After a while looking for another guitarist, we finally decide to stay this aggressive trio, composing songs one at a time ...'
+        'After a while looking for another guitarist, we finally found André to fill this role, composing songs one at a time ...'
       ].join('<br/>\n')),
       members: [{
         name: 'Max',
-        avatar: 'images/profiles/max.png',
+        // avatar: 'images/profiles/max.png',
         bio: $sce.trustAsHtml([
           '<code>Max</code> is a long time guitar player ... but alone (or with his brother JoZ at time).',
           'After studies, sport and family, he decided it was the time to form a band.',
           'This is how the story begins.',
-          'As a huge fan of MetallicA and more recently Machine Head, he is the most thrash oriented dude of the trio.'
+          'As a huge fan of <code>MetallicA</code> and more recently <code>Machine Head</code>, his play is really thrash oriented.'
         ].join('<br/>\n')),
         instruments: ['Guitar', 'Vocals'],
         gears: [
@@ -34,8 +34,9 @@ angular.module('controllers')
           {
             type: 'Amplifiers',
             gears: [
-              'Marshall JCM 2000 DSL 401',
-              'Peavey Ultra+ w/ Marshall 1960AV Cabinet'
+              'Peavey XXX II w/ Marshall 1960AV Cabinet',
+              'Peavey Ultra+ w/ Marshall 1960AV Cabinet',
+              'Marshall JCM 2000 DSL 401'
             ]
           },
           {
@@ -49,7 +50,9 @@ angular.module('controllers')
               'MXR Boost MC-401',
               'G-Lab Smooth Delay SD-1',
               'Morley Alligator Volume',
-              'Boss Metal Zone MT-2'
+              'Visual Sound Garagetone Tremolo',
+              'MXR 10-Band Graphic EQ M108'
+              // 'Boss Metal Zone MT-2'
             ]
           },
           {
@@ -60,9 +63,17 @@ angular.module('controllers')
             ]
           },
           {
+            type: 'Power supply',
+            gears: [
+              'Flatliner SIX',
+              'G-Lab 8 Separated 9V Power Box PB-1'
+            ]
+          },
+          {
             type: 'Other',
             gears: [
-              'tc electronic Polytune Tuner',
+              // 'tc electronic Polytune Tuner',
+              'Planet Waves PW-CT-11 Tru-Strobe Tuner',
               'ISP Noise Gate Decimator G String'
             ]
           }
@@ -70,7 +81,7 @@ angular.module('controllers')
       },
       {
         name: 'David',
-        avatar: 'images/profiles/david.png',
+        // avatar: 'images/profiles/david.png',
         bio: $sce.trustAsHtml([
           'Coming soon ...'
         ].join('<br/>\n')),
@@ -84,7 +95,7 @@ angular.module('controllers')
       },
       {
         name: 'Benji',
-        avatar: 'images/profiles/benji.png',
+        // avatar: 'images/profiles/benji.png',
         bio: $sce.trustAsHtml([
           'Coming soon ...'
         ].join('<br/>\n')),
@@ -97,6 +108,25 @@ angular.module('controllers')
           {
             type: 'Effects',
             gears: ['Dunlop Bass Q Crybaby Wha', 'Bass Overdrive Boss ODB-3']
+          }
+        ]
+      },
+      {
+        name: 'André',
+        // avatar: 'images/profiles/andre.jpg',
+        bio: $sce.trustAsHtml([
+          "Guitarist for 10 years, I've played in a band named <code>Raven Fly</code> before I've been in <code>Whö's your Daddy?</code>",
+          "My favorite bands are <code>Children of Bodom</code>, <code>Arch Enemy</code> and <code>Born of Osiris</code>, that have a great influence on my play!"
+        ].join('<br/>\n')),
+        instruments: ['Guitar'],
+        gears: [
+          {
+            type: 'Guitars',
+            gears: ['Jackson KVX10 King V', 'ESP LTD M-400']
+          },
+          {
+            type: 'Amplifiers',
+            gears: ['Marshall MG 100-DFX']
           }
         ]
       }]
